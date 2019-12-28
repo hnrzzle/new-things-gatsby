70cafdd26b97f494bec3a391575471b55e865d06
hnrzzle <henrique.j.oliveira1@gmail.com>
2019-12-28 03:10:19 -0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,23 @@ module.exports = {
         normalizer: function({ entities }) {
           return entities;
         },
-
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-155087689-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
       },
     },
     {
